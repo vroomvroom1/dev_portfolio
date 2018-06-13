f77b95a460c10a51de1f668b0760cf4a07e36379
Matt <mattvroom1@gmail.com>
2018-06-13 16:28:35 -0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
 #route to pages located inside controller with 'about' being in url, to: is location
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+
   resources :blogs
 
 #homepage route
