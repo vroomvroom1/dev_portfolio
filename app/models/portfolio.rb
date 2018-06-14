c4fef465e0f443bcc9d77bb702a404c5895d1a62
Matt <mattvroom1@gmail.com>
2018-06-14 15:40:20 -0500

    image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,13 @@ class Portfolio < ApplicationRecord
   def self.React
     where(subtitle: 'React')
   end
+
+
+#default images for items
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
