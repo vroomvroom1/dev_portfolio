9026cd6ccf3d860b2c044fdc25ea6975dbdcd0b9
Matt <mattvroom1@gmail.com>
2018-06-15 22:45:04 -0500

    Refactored application controller with concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include SetSource
 
-  before_action :set_source
-
-
-#session data is saved between pages
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
 end
