e581dcbbdc0056051d2b45860e0fffe9c5c80035
Matt <mattvroom1@gmail.com>
2018-07-05 16:57:33 -0500

    First update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,7 +6,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :react], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.order("position ASC")
   end
 
   def React
