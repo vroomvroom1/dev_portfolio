97de8e5bf2c350120a47c27bc698083645a209ac
Matt <mattvroom1@gmail.com>
2018-07-16 21:37:22 -0500

    Authorization for drag and drop

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@
 class PortfoliosController < ApplicationController
   before_action :show_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index, :react], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :react], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.by_position
